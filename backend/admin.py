445a5efe90957f84a2bacaed71353c9aa55159d2
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-17 17:59:55 +0530

    BookAuthorInline added to admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Author, Book
+from .models import Category, Author, Book, BookAuthor
 
 
 # Register your models here.
@@ -11,8 +11,15 @@ class CategoryAdmin(admin.ModelAdmin):
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ('first_name', 'last_name',)
 
+
+class BookAuthorInline(admin.TabularInline):
+    model = BookAuthor
+    extra = 1  # Show 1 empty row to add new relationships
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+
+    inlines = [BookAuthorInline]
 
     list_display = ('title', 'category', 'publication_date', 'copies_owned')
 
