7392e70820d0d7dfe5bf42b089987539d304bd90
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-17 17:35:40 +0530

    Category model added

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "category"
