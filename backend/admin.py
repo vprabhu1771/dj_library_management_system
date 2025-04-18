36e24f0f8f43437672fc9ac414d2e44c4fafed26
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-18 10:03:08 +0530

    Customer User Added to Admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,8 +1,48 @@
 from django.contrib import admin
+
+from django.contrib.auth.admin import UserAdmin
+
+from backend.forms import CustomUserCreationForm, CustomUserChangeForm
+from backend.models import CustomUser
+from django.utils.html import format_html
+
 from .models import Category, Author, Book, BookAuthor
 
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+
+    form = CustomUserChangeForm
+
+    model = CustomUser
+
+    list_display = ('email', 'gender', 'image_tag', 'is_staff', 'is_active',)
+
+    list_filter = ('email', 'is_staff', 'is_active',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'gender', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'gender', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+
+    search_fields = ('email',)
+
+    ordering = ('email',)
+
+    def image_tag(self, obj):
+        return format_html('<img src ="{}" width ="150" height="150" />'.format(obj.image.url))
+
+    image_tag.short_description = 'Image'
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
