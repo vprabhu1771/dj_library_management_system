35fbd509ba6913356ea2d571c4b2efb2cdc0624f
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-18 19:57:08 +0530

    member_register, member_login logic added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from frontend.views import home
+from frontend.views import home, member_register, member_login
 
 urlpatterns = [
     path('', home),
+
+    path('register/', member_register, name='register'),
+
+    path('login/', member_login, name='login'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+
+from frontend.forms import RegisterForm, LoginForm
+
 
 # Create your views here.
 def home(request):
     return render(request, "frontend/home.html")
+
+# Member Registration
+def member_register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('member_dashboard')
+    else:
+        form = RegisterForm()
+    return render(request, 'frontend/register.html', {'form': form})
+
+# Member Login
+def member_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                return redirect('member_dashboard')
+    else:
+        form = LoginForm()
+    return render(request, 'frontend/login.html', {'form': form})
