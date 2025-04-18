0f7c782c62b6e9cf3dec1d2c73e765954ca10133
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-18 10:21:21 +0530

    Loan model added

--- backend/models.py
+++ backend/models.py
@@ -116,3 +116,17 @@ class BookAuthor(models.Model):
 
     def __str__(self):
         return f"{self.author} - {self.book}"
+
+
+class Loan(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    member = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    loan_date = models.DateField()
+    returned_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.member.email} - {self.book.title}"
+
+    class Meta:
+        db_table = 'loan'
