a0c101f51d0feea6e544483c01bd9db099c0af44
vprabhu1771 <vprabhu1771@gmail.com>
2025-04-18 10:02:45 +0530

    Custom User Logic Implemented

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,60 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from backend.manager import CustomerUserManager
+
 # Create your models here.
+
+class Gender(models.TextChoices):
+    MALE = 'M', _('Male')
+    FEMALE = 'F', _('Female')
+
+class GenderedImageField(models.ImageField):
+
+    def pre_save(self, model_instance, add):
+        value = super().pre_save(model_instance, add)
+        if not value or not hasattr(model_instance, self.attname):
+            # If no image provided or new instance
+            # default gender
+            gender = model_instance.gender if hasattr(model_instance, 'gender') else Gender.MALE
+            if gender == Gender.MALE:
+                value = 'profile/male_avatar.png'
+            elif gender == Gender.FEMALE:
+                value = 'profile/female_avatar.png'
+            else:
+                # fallback default image
+                value = 'profile/default_image.jpg'
+
+        elif model_instance.gender != getattr(model_instance, f"{self.attname}_gender_cache", None):
+            # If gender has changed
+            gender = model_instance.gender
+            if gender == Gender.MALE:
+                value = 'profile/male_avatar.png'
+            elif gender == Gender.FEMALE:
+                value = 'profile/female_avatar.png'
+            else:
+                # fallback default image
+                value = 'profile/default_image.jpg'
+        setattr(model_instance, f"{self.attname}_gender_cache", model_instance.gender)
+        return value
+
+class CustomUser(AbstractUser):
+    username = None
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(_('email address'),unique=True)
+    gender = models.CharField(max_length=1,choices=Gender.choices,default=Gender.MALE)
+    image = GenderedImageField(upload_to='profile/',blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'gender']
+    objects = CustomerUserManager()
+
+    def __str__(self):
+        return self.email
+
 class Category(models.Model):
     id = models.BigAutoField(primary_key=True)
 
--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = 'media/'
+
+AUTH_USER_MODEL = 'backend.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+
+    )
+}
